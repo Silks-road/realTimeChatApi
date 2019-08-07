d282d2f6457f6b39d0be51bee9d6955947ae6172
merve.silk <secondstarfrmtherite@gmail.com>
2019-08-07 19:42:36 +0100

    Updated/ modified index files, created .tsx files

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,13 @@ app.get("/", (request, response) => {
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`)
 });
+
+
+// 
+// // These should be at the top of the file
+// import Bundler from "parcel-bundler";
+// import path from "path";
+//
+// // replace the call to app.get with:
+// const bundler = new Bundler(path.join(__dirname, "../src/client/index.html"));
+// app.use(bundler.middleware());
